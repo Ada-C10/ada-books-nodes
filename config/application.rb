243911641bcd7e2b5a1516c7397253a8a147d037
Devin Helmgren <devincollege@gmail.com>
2018-09-17 10:35:31 -0700

    Rails setup

---
+++ config/application.rb
@@ -0,0 +1,26 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module AdaBooksNodes
+  class Application < Rails::Application
+    config.generators do |g|
+      # Force new test files to be generated in the minitest-spec style
+      g.test_framework :minitest, spec: true
+
+      # Always use .js files, never .coffee
+      g.javascript_engine :js
+    end
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+  end
+end
