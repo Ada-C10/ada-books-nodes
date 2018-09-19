1a7bd00be099272e14d01e80184dea50f9eefae3
Devin Helmgren <devincollege@gmail.com>
2018-09-19 15:09:19 -0700

    Created a form to add a new thing to the DB. It used New and Create in Books Controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,6 +20,12 @@ class BooksController < ApplicationController
   end
 
   def create
+    @book = Book.new(author: params[:book][:author], title: params[:book][:title]) #instantiate a new book
+    if @book.save # save returns true if the database insert succeeds
+      redirect_to root_path # go to the index so we can see the book in the list
+    else # save failed :(
+      render :new # show the new book form view again
+    end
   end
 
   def new
