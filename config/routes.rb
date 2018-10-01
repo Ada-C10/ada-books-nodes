c65db67e466373b7591166b1a25172653aa3f4d3
CheezItMan <chris@adadevelopersacademy.org>
2018-10-01 10:50:53 -0700

    added resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,23 @@
 Rails.application.routes.draw do
   root "books#index"
 
-  get '/books', to: 'books#index', as: 'books'
+  resources :books
 
-  get '/books/new', to: 'books#new', as: 'new_book'
-  post '/books', to: 'books#create'
-
-  get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
-
-  get '/books/:id', to: 'books#show', as: 'book'
-
-  patch '/books/:id', to: 'books#update'
-  put '/books/:id', to: 'books#update'
-
-  delete '/books/:id', to: 'books#destroy'
+  # get '/books', to: 'books#index', as: 'books'
+  #
+  # get '/books/new', to: 'books#new', as: 'new_book'
+  # post '/books', to: 'books#create'
+  #
+  # get '/books/:id/edit', to: 'books#edit', as: 'edit_book'
+  #
+  # get '/books/:id', to: 'books#show', as: 'book'
+  #
+  # patch '/books/:id/complete', to: 'books#mark_complete', as: 'mark_complete'
+  #
+  # patch '/books/:id', to: 'books#update', as: 'update_book'
+  # put '/books/:id', to: 'books#update'
+  #
+  # delete '/books/:id', to: 'books#destroy'
 
   #delete '/books/:id', to: 'books#destroy'
 
