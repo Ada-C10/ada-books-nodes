35404240cd6a1b3d70d790172c853fecc1378f45
CheezItMan <chris@adadevelopersacademy.org>
2018-10-01 13:06:53 -0700

    added relationship

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -57,7 +57,7 @@ class BooksController < ApplicationController
   private
 
     def book_params
-      return params.require(:book).permit(:title, :author, :description)
+      return params.require(:book).permit(:title, :author_id, :description)
     end
 
 
---
+++ db/migrate/20181001181246_remove_author_from_books.rb
@@ -0,0 +1,5 @@
+class RemoveAuthorFromBooks < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :books, :author
+  end
+end
