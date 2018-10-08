19baa55a9bc7f17bd97b2776a4c55ebfc4d742ed
CheezItMan <chris@adadevelopersacademy.org>
2018-10-08 09:35:18 -0700

    finished testing books

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -6,4 +6,31 @@ describe Book do
   it "must be valid" do
     expect(book).must_be :valid?
   end
+
+  it "is invalid without an author" do
+    book.author_id = nil
+
+    expect(book.valid?).must_equal false
+    expect(book.errors.messages).must_include :author
+  end
+
+  it "is invalid without a title" do
+    book.title = nil
+
+    expect(book.valid?).must_equal false
+    expect(book.errors.messages).must_include :title
+  end
+
+  it "must have an author" do
+
+    expect(book.author).must_be_instance_of Author
+  end
+
+  it "must have the required fields" do
+    fields = [:title, :description, :author, :date_published]
+
+    fields.each do |field|
+      expect(book).must_respond_to field
+    end
+  end
 end
