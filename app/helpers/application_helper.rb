7a9df1cf048eb5c9ff3c375e66cdfe8aa7b7dd62
Devin Helmgren <devincollege@gmail.com>
2018-10-19 09:45:50 -0700

    Added view helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def readable_date(date)
+    ("<span class='date'>" + date.strftime("%A, %b %d") + "</span>").html_safe
+  end
+  def random_image(width, height)
+    number = rand(3)
+    url = ""
+    case number
+    when 0
+      url = "https://picsum.photos/"
+    when 1
+      url = "http://www.placecage.com/"
+    when 2
+      url = "http://placekitten.com/"
+    end
+    ["<img src='", url, width,"/" ,height, "' alt='random image'>"].join.html_safe
+  end
 end
